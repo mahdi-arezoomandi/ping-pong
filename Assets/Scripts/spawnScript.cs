fcec878283aa281c93d60b32f868f692c5bd1270
mahdi-arezoomandi <mahdiarezand@gmail.com>
2025-04-22 18:41:30 +0430

    update spawn script

--- Assets/Scripts/spawnScript.cs
+++ Assets/Scripts/spawnScript.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class spawnScript : MonoBehaviour
 {
+    public GameObject Ball;
+    private float Position;
+    public float time = 0;
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,20 @@ public class spawnScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Position = Random.Range(-4,4);
+        clon();
+
+    }
+    private void clon()
+    {
+
+        if (time < 3)
+            time = time + Time.deltaTime;
+        else
+        {
+            Instantiate(Ball, new Vector2(transform.position.x, Position), transform.rotation);
+            time = 0;
+        }
+
     }
 }
