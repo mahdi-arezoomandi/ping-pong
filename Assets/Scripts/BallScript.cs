72ae0b37eb85bc8b0c6c79e2e89e22c6c921ce7d
mahdi-arezoomandi <mahdiarezand@gmail.com>
2025-04-23 18:39:15 +0430

    Update BallScript.cs

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -7,14 +7,16 @@ public class BallScript : MonoBehaviour
     public Rigidbody2D Rigidball;
     private float ballspeedX;
     private float ballspeedY;
+    private float ballspeed;
     public float speed = 50;
 
     // Start is called before the first frame update
     void Start()
     {
         ballspeedX = Random.Range(-5, 5);
+        ballspeedX = Random.Range(-5, 5);
         ballspeedY = Random.Range(-5, 5);
-        Rigidball.AddForce(new Vector2(ballspeedX*50, ballspeedY));
+        Rigidball.AddForce(new Vector2(ballspeedX*50, ballspeedY*50));
     }
 
     // Update is called once per frame
@@ -23,5 +25,10 @@ public class BallScript : MonoBehaviour
         if(transform.position.x<-9||transform.position.x>9)
             GameObject.Destroy(gameObject);
         
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            Rigidball.AddForce(new Vector2(9*50,ballspeed*50));
     }
 }
