8b42e0d5323868200aea91bbb835251dcd2d4633
mahdi-arezoomandi <mahdiarezand@gmail.com>
2025-04-23 18:05:12 +0430

    add Ball Script

--- Assets/Scripts/spawnScript.cs
+++ Assets/Scripts/spawnScript.cs
@@ -9,7 +9,7 @@ public class spawnScript : MonoBehaviour
     private float Position;
     private float SpeedBall;
     public float time = 0;
-    private bool Ballisalive=false;
+    
    
     // Start is called before the first frame update
     void Start()
@@ -20,22 +20,23 @@ public class spawnScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       SpeedBall = Random.Range(-4, 4);
-        Position = Random.Range(-4,4);
+      
         clon();
-        if (Ball.transform.position.x < -9 || Ball.transform.position.x > -9)
-            GameObject.Destroy(Ball);
-        BallRigid.AddForce(new Vector2(SpeedBall, 0f));
+        
+        
 
     }
     private void clon()
     {
+       
+        Position = Random.Range(-4, 4);
 
         if (time < 3)
             time = time + Time.deltaTime;
         else
         {
             Instantiate(Ball, new Vector2(transform.position.x, Position), transform.rotation);
+            
             time = 0;
         }
 
