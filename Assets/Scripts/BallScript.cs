bfbb1048d0803510b0dc4467153ef2eca7524ce2
mahdi-arezoomandi <mahdiarezand@gmail.com>
2025-04-25 17:04:20 +0430

    update ball script

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -7,33 +7,44 @@ public class BallScript : MonoBehaviour
     public Rigidbody2D Rigidball;
     private float ballspeedX;
     private float ballspeedY;
-    private float ballspeed;
-    public float speed = 50;
+    private float ballspeeda;
+    private float ballspeedb;
+    public float speed ;
    
-   
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        ballspeedX = Random.Range(-5, 5);
-        ballspeedX = Random.Range(-5, 5);
-        ballspeedY = Random.Range(-5, 5);
-        
-        Rigidball.AddForce(new Vector2(ballspeedX*50, ballspeedY*50));
-    }
+        ballspeeda =Random.Range(0f,1f);
+        ballspeedb = Random.Range(0f, 1f);
+        if (ballspeeda > 0.5f)
+            ballspeedX = 5;
+        else
+            ballspeedX = -5;
+        if (ballspeedb > 0.5f)
+            ballspeedY = Random.Range(2, 5);
+        else
+            ballspeedY = Random.Range(-5, -2);
 
+        SBall(ballspeedX, ballspeedY);
+               
+       
+      
+
+    }
+    
     // Update is called once per frame
     void Update()
     {
         if(transform.position.x<-9||transform.position.x>9)
-            GameObject.Destroy(gameObject);
+            Destroy(gameObject);
         
     }
-    private void OnCollisionEnter(Collision collision)
+  
+    public virtual void SBall(float x,float y)
     {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            Rigidball.AddForce(new Vector2(9 * 50, ballspeed * 50));
-            
-        }
+        Rigidball.velocity=new Vector2(x * speed, y * speed);
     }
 }
